044660e6b1a293487c87481438d98f2e2d1d4dd9
savor1984 <savor1984@126.com>
2014-04-27 18:15:56 +0800

    111

--- js/index.jquery.js
+++ js/index.jquery.js
@@ -22,7 +22,7 @@ $(function() {
 			}
 			
 			for(var i = 1;i <= dir;i++){
-				$("#tiles").append('<li><a href="images/scale/'+page+'/large/'+i+'.jpg"  title=""><img src="images/scale/'+page+'/small/'+i+'.jpg" width="200" height="'+rnd(300,400)+'"></a></li>');
+				$("#tiles").append('<li id="photo"><a href="images/scale/'+page+'/large/'+i+'.jpg"  title=""><img src="images/scale/'+page+'/small/'+i+'.jpg" width="200" height="'+rnd(300,400)+'"></a></li>');
 			}
 			
 			setupZoom();
@@ -44,6 +44,10 @@ $(function() {
 
 	     	})(jQuery);
 		  }
+		  
+		  $("img").load(function(){
+		    $(".photo").text("Image loaded");
+		  });
 		        
 	      if(statusTxt=="error")
 		  
