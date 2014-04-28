255b45f7d8359dbd5c2b49e0a8a9d4461320fe87
savor1984 <savor1984@126.com>
2014-04-28 22:55:21 +0800

    111

--- js/index.jquery.js
+++ js/index.jquery.js
@@ -30,7 +30,7 @@ $(function() {
 		  	
 			for(var i = 1;i <= dir;i++){
 				$("#tiles").append('<li id="photo"><a href="images/scale/'+page+'/large/'+i+'.jpg"  title=""><img id='+i+' src="images/scale/'+page+'/small/'+i+'.jpg" style="width:200px;max-height:500px;"></a></li>');
-				alert($('#'+i).height());
+				alert(i);
 			}
 			
 			
